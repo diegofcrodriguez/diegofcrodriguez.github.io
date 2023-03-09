d31937402358c5a36c8b61e2f1f70c4bfc01d0a8
diegofcrodriguez <127364918+diegofcrodriguez@users.noreply.github.com>
2023-03-08 22:53:25 -0300

    Initial commit

---
+++ funciones.js
@@ -0,0 +1,9 @@
+document.addEventListener('click',function(){
+    document.getElementById('JRR').style.backgroundColor='white';
+    document.getElementById('JRR').style.borderColor='lightskyblue';    
+
+})
+document.addEventListener('dblclick',function(){
+    document.getElementById('JRR').style.backgroundColor='gold';
+    document.getElementById('JRR').style.borderColor='darkblue';     
+})
